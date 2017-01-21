603678925ba603548f6bbe94ac39c4ffbd3cc9db
Mackenzie Drew <mackiedrew@gmail.com>
2017-01-21 02:39:51 -0500

    Added power settings, removed sudo, added Inkscape

--- setup.sh
+++ setup.sh
@@ -8,81 +8,90 @@ read git_email
 
 #### Housekeeping ####
 cwd=$(pwd)
-sudo apt-get update
-sudo apt-get install -f
+
+apt-get update
+apt-get install -f
 source ~/.bashrc
+
+#### Power ####
+gsettings set org.cinnamon.settings-daemon.plugins.power time-critical 900
+gsettings set org.cinnamon.settings-daemon.plugins.power percentage-critical 10
 
 #### Google Chrome ####
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo rm google-chrome-stable_current_amd64.deb
-sudo apt-get -f install
+dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+apt-get -f install
 cd $cwd
 
 #### Text Editors ####
-sudo apt-get install -y vim
-sudo apt-get install -f
+apt-get install -y vim
+apt-get install -f
 wget -c https://github.com/atom/atom/releases/download/v1.11.2/atom-amd64.deb
-sudo dpkg -i atom-amd64.deb
-sudo rm atom-amd64.deb
+dpkg -i atom-amd64.deb
+rm atom-amd64.deb
 cd $cwd
-sudo apm update
-sudo apm install linter
-sudo apm install linter-pylint
-sudo apm install vim-mode
-sudo apm install project-manager
-sudo apm install python-tools
-sudo apm install python-indent
-sudo apm install python-snippets
-sudo apm install autocomplete-python
+
+#### Install Atom Packages ####
+apm update
+apm install linter
+apm install linter-pylint
+apm install vim-mode
+apm install project-manager
+apm install python-tools
+apm install python-indent
+apm install python-snippets
+apm install autocomplete-python
 
 #### GIT ####
-sudo apt-get install -y git
-sudo apt-get install -fy
+apt-get install -y git
+apt-get install -fy
 git config --global user.name $git_name
 git config --global user.email $git_email
 git config --global push.default matching
 git config --global core.editor "vim"
 
 #### JavaScript ####
-sudo apt-get install -y npm
-sudo apt-get install -f
-sudo apt-get install -y nodejs
-sudo apt-get install -f
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-sudo apt-get install -y gulp
+apt-get install -y npm
+apt-get install -f
+apt-get install -y nodejs
+apt-get install -f
+ln -s /usr/bin/nodejs /usr/bin/node
+apt-get install -y gulp
 
 #### NPM Modules ####
-sudo npm install -g gulp
-sudo npm install -g eslint
-sudo npm install -g eslint-plugin-babel
-sudo npm install -g babel-eslint
-sudo npm install -g eslint-config-airbnb
-sudo npm install -g eslint-plugin-jsx-a11y
-sudo npm install -g eslint-plugin-react
-sudo npm install -g eslint-plugin-import
+npm i -g gulp
+npm i -g eslint
+npm i -g eslint-plugin-babel
+npm i -g babel-eslint
+npm i -g eslint-config-airbnb
+npm i -g eslint-plugin-jsx-a11y
+npm i -g eslint-plugin-react
+npm i -g eslint-plugin-import
 
 #### Easy Programs ####
-sudo apt-get install -y gimp
-sudo apt-get install -f
-sudo apt-get install -y trash-cli
-sudo apt-get install -f
-sudo apt-get install -y vlc-nox
-sudo apt-get install -f
-sudo apt-get install -y audacity
-sudo apt-get install -f
-sudo apt-get install -y python3-pip
-sudo apt-get install -f
-sudo apt-get install -y gparted
-sudo apt-get install -f
-sudo apt-get install -y p7zip-full
-sudo apt-get install -f
-sudo apt-get install -y virtualbox
-sudo apt-get install -f
+apt-get install -y inkscape
+apt-get install -f
+apt-get install -y gimp
+apt-get install -f
+apt-get install -y trash-cli
+apt-get install -f
+apt-get install -y vlc-nox
+apt-get install -f
+apt-get install -y audacity
+apt-get install -f
+apt-get install -y python3-pip
+apt-get install -f
+apt-get install -y gparted
+apt-get install -f
+apt-get install -y p7zip-full
+apt-get install -f
+apt-get install -y virtualbox
+apt-get install -f
 
 # Setup Aliases
 cd $cwd
 cp -v -f ./aliases.txt ~/
 mv ~/aliases.txt ~/.aliases
-sudo echo "source ~/.aliases" >> ~/.bashrc
-sudo source ~/.bashrc
+echo "source ~/.aliases" >> ~/.bashrc
+source ~/.bashrc
